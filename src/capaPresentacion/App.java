6f8a46d8843c72c109d53f4044acf683ddc6dc1a
alonz <alonz@Ignacio>
2024-04-09 07:36:18 -0500

    añadir la clase apellido

--- src/capaPresentacion/App.java
+++ src/capaPresentacion/App.java
@@ -14,6 +14,6 @@ public class App {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");    }
+        System.out.println("prueba");    }
     
 }
